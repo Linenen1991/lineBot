5306b9b12afdd21e8d76b5f65e2d1b4f5f3a9be3
Linenen1991 <110909635+Linenen1991@users.noreply.github.com>
2022-08-09 22:25:07 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,48 @@
-// index.jsconst line = require('@line/bot-sdk');
-var express = require('express');const config = {
-  channelAccessToken: 'YOUR_CHANNEL_ACCESS_TOKEN',
-  channelSecret: 'YOUR_CHANNEL_SECRET'
-};// create LINE SDK client
-const client = new line.Client(config);// create Express app
-// about Express itself: <https://expressjs.com/>
-const app = express();// register a webhook handler with middleware
+'use strict';
+
+const line = require('@line/bot-sdk');
+const express = require('express');
+
+// create LINE SDK config from env variables
+const config = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET,
+};
+
+// create LINE SDK client
+const client = new line.Client(config);
+
+// create Express app
+// about Express itself: https://expressjs.com/
+const app = express();
+
+// register a webhook handler with middleware
 // about the middleware, please refer to doc
 app.post('/callback', line.middleware(config), (req, res) => {
-	console.log(req, res)  Promise
+  Promise
     .all(req.body.events.map(handleEvent))
     .then((result) => res.json(result))
     .catch((err) => {
       console.error(err);
       res.status(500).end();
     });
-});// event handler
+});
+
+// event handler
 function handleEvent(event) {
   if (event.type !== 'message' || event.message.type !== 'text') {
     // ignore non-text-message event
     return Promise.resolve(null);
-  }  // create a echoing text message
-  const echo = { type: 'text', text: event.message.text };  // use reply API
+  }
+
+  // create a echoing text message
+  const echo = { type: 'text', text: event.message.text };
+
+  // use reply API
   return client.replyMessage(event.replyToken, echo);
-}// listen on port
+}
+
+// listen on port
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`listening on ${port}`);
